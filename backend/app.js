283093889596e172af0a5eeafa8187dc3c635072
Gaushmohammad Siddiqui <gmsiddiqui36@gmail.com>
2026-01-22 03:53:41 +0530

    Updated Build

--- backend/app.js
+++ backend/app.js
@@ -67,7 +67,8 @@ const path = require('path');
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'dist')));
 
-  app.get('/*', (req, res) => {
+  app.use((req, res, next) => {
+    if (req.path.startsWith('/api')) return next();
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
   });
 }
